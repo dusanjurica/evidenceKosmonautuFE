dd18068a53925590c9096e25a9f8652261c02c41
djurica <djurica@DESKTOP-KQ6905R>
2021-02-19 15:00:05 +0100

    Fronend aplikace evidence kosmonautu

---
+++ Kosmonauti/Data/KosmonautDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kosmonauti.Data
+{
+    public class KosmonautDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Jmeno { get; set; }
+
+        [Required]
+        public string Prijmeni { get; set; }
+
+        [Required]
+        public DateTime DatumNar { get; set; }
+
+        public IList<string> Superschopnosti { get; set; }
+
+        public IList<int> SuperschopnostiIds { get; set; }
+    }
+}
---
+++ Kosmonauti/Services/KosmonautService.cs
@@ -0,0 +1,17 @@
+﻿using Kosmonauti.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Kosmonauti.Services
+{
+    public class KosmonautService
+    {
+        public void Add(KosmonautDTO newKosmonaut)
+        {
+            Debug.WriteLine("adding a new kosmonaut dto to a database");
+        }
+    }
+}
